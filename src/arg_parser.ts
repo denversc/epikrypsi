f0c5249cbe02c56e7e569f5368b3b8d4efd4c666
denversc <github.prefix924@passmail.com>
2025-05-29 16:37:10 -0400

    src/arg_parser.ts: add --no-warn-about-password-on-command-line

--- src/arg_parser.ts
+++ src/arg_parser.ts
@@ -7,12 +7,18 @@ import { ConstantPasswordProvider, ReadlinePasswordProvider } from "./passwords.
 import { type Commands } from "./command_types.ts";
 
 export default async function parseArgs(commands: Commands, logger: Logger): Promise<void> {
-  const yargsParser = yargs(hideBin(process.argv))
+  const yargsParser = yargs(hideBin(process.argv));
+
+  const yargsCommands = yargsParser
     .scriptName("epk")
-    .version(version)
     .usage("Usage: $0 <command> [options]")
+    .version(version)
+    .strict()
+    .showHelpOnFail(false, "Specify --help for help")
+    .epilogue(`Each boolean argument can be negated by prefixing it with "no-".`)
+    .wrap(yargsParser.terminalWidth())
     .command({
-      command: "init <file>",
+      command: "init <file> [options]",
       describe: "Create a new Epikrypsi volume",
       builder: yargs =>
         yargs
@@ -25,18 +31,27 @@ export default async function parseArgs(commands: Commands, logger: Logger): Pro
             alias: "p",
             string: true,
             describe: "The password to use for the new Epikrypsi volume",
+          })
+          .option("warn-about-password-on-command-line", {
+            boolean: true,
+            default: true,
+            describe:
+              "Whether to emit a warning when the --password command-line argument is used.",
           }),
       handler: argv => {
-        const { file, password } = argv;
-        const passwordProvider = passwordProviderFromArguments({ password }, logger);
+        const { file, password, warnAboutPasswordOnCommandLine } = argv;
+        const passwordProvider = passwordProviderFromArguments(
+          { password, warnAboutPasswordOnCommandLine },
+          logger,
+        );
         return commands.init({ args: { file, passwordProvider }, logger });
       },
-    })
-    .showHelpOnFail(false, "Specify --help for help")
-    .strict();
+    });
 
-  const parsedArguments = await yargsParser.parse();
+  // Parse the arguments and run the corresponding command.
+  const parsedArguments = await yargsCommands.parse();
 
+  // If no command was specified, just show the help screen then exit.
   if (parsedArguments._.length === 0) {
     yargsParser.showHelp("log");
   }
@@ -45,19 +60,23 @@ export default async function parseArgs(commands: Commands, logger: Logger): Pro
 function passwordProviderFromArguments(
   arguments_: {
     password: string | undefined;
+    warnAboutPasswordOnCommandLine: boolean;
   },
   logger: Logger,
 ) {
-  const { password } = arguments_;
+  const { password, warnAboutPasswordOnCommandLine } = arguments_;
   const trimmedPassword = password?.trim();
   if (typeof trimmedPassword === "string" && trimmedPassword.length > 0) {
-    logger.warn(
-      `WARNING: specifying a password on the command line is insecure and should not be done ` +
-        `with real passwords that require secrecy. This is because command line arguments are ` +
-        `often stored in plain text in "history" files by shells, and also are usually visible ` +
-        `by all users in the operating system's list of processes, such as the output of the ` +
-        `"top" and "ps" commands on Linux. Prefer using an interactive prompt instead.`,
-    );
+    if (warnAboutPasswordOnCommandLine) {
+      logger.warn(
+        `WARNING: specifying a password on the command line is insecure and should not be done ` +
+          `with real passwords that require secrecy. This is because command line arguments ` +
+          `are often stored in plain text in "history" files by shells, and also are usually ` +
+          `visible by all users in the operating system's list of processes, such as the ` +
+          `output of the "top" and "ps" commands on Linux. Prefer using an interactive prompt ` +
+          `instead.`,
+      );
+    }
     return new ConstantPasswordProvider(trimmedPassword);
   }
   return new ReadlinePasswordProvider({ input: process.stdin, output: process.stdout });
